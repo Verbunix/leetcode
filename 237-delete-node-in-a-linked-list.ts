a13bb3847d72fc8fd84beb783f5e9e617a22a9e5
Andrii Verbytskyi <verb2310@gmail.com>
2022-06-16 23:10:58 +0200

    237. Delete Node in a Linked List

---
+++ 237-delete-node-in-a-linked-list.ts
@@ -0,0 +1,31 @@
+class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+  }
+}
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+/**
+ Do not return anything, modify it in-place instead.
+ */
+function deleteNode(root: ListNode | null): void {
+  if (!root.next) return;
+
+  root.val = root.next.val;
+  root.next = root.next.next;
+  return;
+}
