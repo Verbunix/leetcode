d6b6ae5463735de47829ea5c586b8197a072756d
Andrii Verbytskyi <verb2310@gmail.com>
2022-06-18 14:05:28 +0200

    1004. Max Consecutive Ones III

---
+++ 1004-max-consecutive-ones-iii.ts
@@ -0,0 +1,22 @@
+function longestOnes(nums: number[], k: number): number {
+  let maxLong = 0;
+
+  for (let right = 0, left = 0, zeros = 0; right < nums.length; right++) {
+    if (nums[right] === 0) {
+      if ((nums.length - right) < maxLong) return maxLong;
+
+      while (zeros >= k) {
+        if (nums[left] === 0) zeros--;
+        left++;
+      }
+      zeros++;
+    }
+    maxLong = Math.max(maxLong, (right - left) + 1);
+  }
+
+  return maxLong;
+}
+
+// console.log(longestOnes([0, 0, 1, 1, 1, 0, 0], 0)); // 3
+// console.log(longestOnes([1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], 2)); // 6
+// console.log(longestOnes([0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1], 3)); // 10
