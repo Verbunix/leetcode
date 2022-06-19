cdb15b5025ddf01c8443476ffd0d4f4c8ed31cf3
Andrii Verbytskyi <verb2310@gmail.com>
2022-06-19 21:11:12 +0200

    104. Maximum Depth of Binary Tree

---
+++ 104-maximum-depth-of-binary-tree.ts
@@ -0,0 +1,40 @@
+class TreeNode {
+  val: number
+  left: TreeNode | null
+  right: TreeNode | null
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+  }
+}
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+function maxDepth(root: TreeNode | null): number {
+  if (!root) return 0;
+
+  function depthFirstSearch(node: TreeNode, acc) {
+    if (!node) return acc;
+    return Math.max(
+      depthFirstSearch(node.left, acc + 1),
+      depthFirstSearch(node.right, acc + 1)
+    );
+  }
+
+  return depthFirstSearch(root, 0);
+}
+
+// console.log(maxDepth([1, 2, 3, 4, 5])); // 3
+// console.log(maxDepth([3, 9, 20, null, null, 15, 7])); // 3
+// console.log(maxDepth([1, null, 2])); // 2
