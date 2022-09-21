296b3560963c0c77f42191a8232f76bc81bc1981
Andrii Verbytskyi <verb2310@gmail.com>
2022-09-21 09:52:20 +0200

    69. Sqrt(x)

--- 69-sqrtx.ts
+++ 69-sqrtx.ts
@@ -1,3 +1,17 @@
+// function mySqrt(x: number): number {
+//   return Number((x ** 0.5).toString().split('.')[0]);
+// }
+
 function mySqrt(x: number): number {
-  return Number((x ** 0.5).toString().split('.')[0]);
+  let res = 0;
+
+  while (true) {
+    const y = res * res;
+    if (y > x) return res - 1;
+    if (y === x) return res;
+    res++;
+  }
 }
+
+// console.log(mySqrt(4)); // 2
+// console.log(mySqrt(8)); // 2
